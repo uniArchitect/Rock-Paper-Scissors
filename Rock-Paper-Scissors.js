ab60faef06b70902a59a1c1120c2b2473efa0bfe
uniArchitect <hancho93@gmail.com>
2022-01-09 23:11:14 -0500

    single round draft set up

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -2,11 +2,36 @@ let possOutcomes = ['rock', 'paper', 'scissors'];
 
 //playerSelection - User will input their move here
 function playerSelection() {
-    let selection = window.prompt("What will you choose?");
+    let selection = prompt("Rock, Paper or Scissors?");
     return selection; 
 }
 
 //computerPlay - Computer's random moves
 function computerPlay(possOutcomes) {
     return possOutcomes[Math.floor(Math.random() * possOutcomes.length)];
+}
+
+//Single Round Game
+function oneRound() {
+    playerSelection();
+    computerPlay(possOutcomes);
+    
+    //Capture Play Variables
+    playerMove = playerSelection();
+    computerMove = computerPlay(possOutcomes);
+    
+    if (playerMove === 'Rock') {
+        if (computerMove === 'rock');
+        return "Tie";
+    }
+
+    if (playerMove === 'Rock') {
+        if (computerMove === 'paper');
+        return "You lost...";
+    }
+
+    if (playerMove === 'Rock') {
+        if (computerMove === 'scissors');
+        return "You win!";
+    }
 }
