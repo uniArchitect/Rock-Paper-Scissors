5a699751915ad53f84e2c2b311a4a1141a3be319
uniArchitect <hancho93@gmail.com>
2022-01-09 22:14:14 -0500

    added playerSelection function

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,5 +1,12 @@
 let possOutcomes = ['rock', 'paper', 'scissors'];
 
+//playerSelection - User will input their move here
+function playerSelection() {
+    let selection = window.prompt("What will you choose?");
+    return selection; 
+}
+
+//computerPlay - Computer's random moves
 function computerPlay(possOutcomes) {
     return possOutcomes[Math.floor(Math.random() * possOutcomes.length)];
 }
